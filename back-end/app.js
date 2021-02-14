3b2f66ebbda9777f0764cc3172f95cea604ef550
swang93 <swang93@uw.edu>
2021-02-13 22:08:12 -0800

    started backend for vendor menu; added dotenv as dependency; made database connection module; changed app.js

--- back-end/app.js
+++ back-end/app.js
@@ -1,17 +1,12 @@
-// get the client
-const mysql = require('mysql2');
+'use strict';
 
-// create the connection to database
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'user',
-  password: 'password',
-  database: 'mydb',
-});
+const express = require('express');
+const LOCAL_PORT = 3000;
+const vendorMenu = require('./routes/vendorMenu');
 
-connection.query(
-  'SELECT * FROM MENU JOIN ITEMS_MENU USING (MENU_ID) JOIN ITEMS USING (ITEM_ID) ORDER BY MENU_ID;',
-  function (err, results, fields) {
-    console.log(results); // results contains rows returned by server
-  }
-);
+const app = express();
+
+app.use('/vendor/menu', vendorMenu);
+
+const PORT = process.env.PORT || LOCAL_PORT;
+app.listen(PORT);
