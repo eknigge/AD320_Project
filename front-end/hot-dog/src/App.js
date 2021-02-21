6198df7fe984c8e655dd61cd0befe657b5399e56
swang93 <swang93@uw.edu>
2021-02-20 16:51:05 -0800

    added react-router and restored index and App.js

---
+++ front-end/hot-dog/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import VendorMenu from './components/VendorMenu';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/vendor/menu" component={VendorMenu} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default App;
