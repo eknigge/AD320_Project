3d0d3b3af56ebb9837a9010b6367926109f6497b
swang93 <swang93@uw.edu>
2021-02-21 18:30:36 -0800

    moved Hanna's app inside main app folder lost image folder

--- front-end/hot-dog/src/App.js
+++ front-end/hot-dog/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import VendorMenu from './components/VendorMenu';
+import Main from './customer/components/TheData';
+import Home from './customer/components/Home';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
+        <Route path="/menu" component={Main} />
+        <Route path="/" exact component={Home} />
         <Route path="/vendor/menu/:id" component={VendorMenu} />
       </Switch>
     </BrowserRouter>
