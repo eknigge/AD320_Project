51a5812e1278c3cf361e727711a3a839e5ca4cdb
swang93 <swang93@uw.edu>
2021-03-08 20:29:55 -0800

    added edit cart starter file

---
+++ front-end/hot-dog/src/components/EditCart.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import AdminMain from './AdminMain';
+
+class EditCart extends React.Component {
+  render() {
+    return <div>hello</div>;
+  }
+}
+
+export default EditCart;
